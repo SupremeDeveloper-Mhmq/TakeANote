9bac447a156f703d39f6f8150b2773cac056efd5
Mohammad Hosein Mousavi <mhmousavi.mhmq@outlook.com>
2021-06-28 18:29:49 -0700

    Ui First Step

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountComponent } from './account/account.component';
+import { AddNotesComponent } from './add-notes/add-notes.component';
+import { AuthComponent } from './auth/auth.component';
+import { NotesComponent } from './notes/notes.component';
+import { ReviewComponent } from './review/review.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/Auth', pathMatch: 'full' },
+  { path: 'Auth', component: AuthComponent },
+  { path: 'Account', component: AccountComponent },
+  { path: 'Notes', component: NotesComponent },
+  { path: 'Review', component: ReviewComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotesComponent } from './notes/notes.component';
@@ -8,6 +8,11 @@ import { AddNotesComponent } from './add-notes/add-notes.component';
 import { DeleteNotesComponent } from './delete-notes/delete-notes.component';
 import { ConfigNotesComponent } from './config-notes/config-notes.component';
 import { AuthComponent } from './auth/auth.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AccountComponent } from './account/account.component';
+import { ReviewComponent } from './review/review.component';
+
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,13 +21,18 @@ import { AuthComponent } from './auth/auth.component';
     AddNotesComponent,
     DeleteNotesComponent,
     ConfigNotesComponent,
-    AuthComponent
+    AuthComponent,
+    AccountComponent,
+    ReviewComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
